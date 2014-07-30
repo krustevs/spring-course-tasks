1d719913f771d3eb1f878679f7c6bbee2269af4c
simo <simo@simo-laptop.(none)>
2014-07-30 19:30:18 +0300

    initial commit

---
+++ src/com/vmware/talentboost/spring/MainApp.java
@@ -0,0 +1,19 @@
+package com.vmware.talentboost.spring;
+
+
+public class MainApp {
+	public static void main(String... args) {
+		// Task 1: Instantiate all the classes and run the game.
+		
+		// Task 2: Involve Spring...
+		//		- Include the required libs to the project
+		//		- Create the XML configuration file
+		//		- In the main method create an application context
+		//			and get the game from it.
+		
+		// Task 3*: Switch to annotation based configuration
+		//		- Use @Autowired instead of constructor-arg
+		//		- Enable automatic component scan
+
+	}
+}
